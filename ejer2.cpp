bbab577a118f57faeed711fe90e97c8ba8d5a4cf
abrilcarrillo <carrillomarianelaabril@gmail.com>
2025-06-24 13:09:54 -0300

    Ejer2 -Lista con inicio, final y contador

---
+++ ejer2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+typedef struct tnodo* pnodo;
+
+typedef struct tnodo {
+	int dato;
+	pnodo siguiente;
+};
+
+typedef struct {
+	pnodo inicio;
+	pnodo final;
+	int cantidad;
+} tlista;
+
+void iniciar_lista(tlista &lista) {
+	lista.inicio = NULL;
+	lista.final = NULL;
+	lista.cantidad = 0;
+}
+
+void crear_nodo(pnodo &nuevo) {
+	nuevo = new tnodo;
+	if (nuevo != NULL) {
+		cout << "Ingrese valor: ";
+		cin >> nuevo->dato;
+		nuevo->siguiente = NULL;
+	} else {
+		cout << "Memoria insuficiente" << endl;
+	}
+}
+
+void agregar_final(tlista &lista, pnodo nuevo) {
+	if (lista.inicio == NULL) {
+		lista.inicio = nuevo;
+		lista.final = nuevo;
+	} else {
+		lista.final->siguiente = nuevo;
+		lista.final = nuevo;
+	}
+	lista.cantidad++;
+}
+
+void mostrar_lista(tlista lista) {
+	pnodo i;
+	if (lista.inicio != NULL) {
+		for (i = lista.inicio; i != NULL; i = i->siguiente) {
+			cout << i->dato << " -> ";
+		}
+		cout << "NULL" << endl;
+	} else {
+		cout << "Lista vacia" << endl;
+	}
+}
+
+int cantidad_elementos(tlista lista) {
+	return lista.cantidad;
+}
+
+int main() {
+	tlista lista;
+	iniciar_lista(lista);
+	char opcion;
+	do {
+		cout << "\--- MENU ---" << endl;
+		cout << "1. Agregar al final" << endl;
+		cout << "2. Mostrar lista" << endl;
+		cout << "3. Mostrar cantidad de elementos" << endl;
+		cout << "4. Salir" << endl;
+		cout << "Opcion: ";
+		cin >> opcion;
+		
+		switch (opcion) {
+		case '1': {
+			pnodo nuevo;
+			crear_nodo(nuevo);
+			agregar_final(lista, nuevo);
+			break;
+		}
+		case '2':
+			mostrar_lista(lista);
+			break;
+		case '3':
+			cout << "Cantidad de elementos: " << cantidad_elementos(lista) << endl;
+			break;
+		case '4':
+			cout << "Saliendo..." << endl;
+			break;
+		default:
+			cout << "Opcion invalida" << endl;
+		}
+		
+	} while (opcion != '4');
+	
+	return 0;
+}
