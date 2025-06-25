d4e41ded08a7b72110b85cca98f3602760d80806
abrilcarrillo <carrillomarianelaabril@gmail.com>
2025-06-25 00:35:55 -0300

    ejer7-commit 1 función enigma

---
+++ ejer7.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+class Nodo {
+private:
+	int dato;
+	Nodo* siguiente;
+	
+public:
+	Nodo(int valor) {
+		dato = valor;
+		siguiente = nullptr;
+	}
+	
+	int getDato() {
+		return dato;
+	}
+	
+	Nodo* getSiguiente() {
+		return siguiente;
+	}
+	
+	void setSiguiente(Nodo* sig) {
+		siguiente = sig;
+	}
+};
+
+// funcion enigma
+void enigma(Nodo* nodo) {
+	if (nodo != nullptr) {
+		if (nodo->getSiguiente() == nullptr)
+			cout << nodo->getDato() << endl;
+		else {
+			enigma(nodo->getSiguiente());
+			cout << nodo->getDato() << endl;
+		}
+	}
+}
+
+// funcion para lista
+Nodo* crear_lista_prueba() {
+	
+	Nodo* n1 = new Nodo(6);
+	Nodo* n2 = new Nodo(4);
+	Nodo* n3 = new Nodo(9);
+	Nodo* n4 = new Nodo(5);
+	Nodo* n5 = new Nodo(8);
+	
+	n1->setSiguiente(n2);
+	n2->setSiguiente(n3);
+	n3->setSiguiente(n4);
+	n4->setSiguiente(n5);
+	
+	return n1; // retorna el inicio
+}
+
+int main() {
+	Nodo* lista = crear_lista_prueba();
+	
+	cout << "Impresion de lista en orden inverso (enigma): " << endl;
+	enigma(lista);
+	
+	return 0;
+}
