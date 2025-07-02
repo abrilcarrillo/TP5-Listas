38659f7fb25d3db17f6972902907fff70187dff6
abrilcarrillo <carrillomarianelaabril@gmail.com>
2025-07-01 22:47:22 -0300

    ejer1-commit 3:agregar final y ordenado

--- TP6-Listas Dobles/ejer1.cpp
+++ TP6-Listas Dobles/ejer1.cpp
@@ -30,3 +30,34 @@ void AgregarInicio(pnodo &inicio, pnodo nuevo) {
 		inicio = nuevo;
 	}
 }
+void AgregarFinal(pnodo &inicio, pnodo nuevo) {
+	pnodo i;
+	if (inicio == NULL) {
+		inicio = nuevo;
+	} else {
+		for (i = inicio; i->sig != NULL; i = i->sig);
+		i->sig = nuevo;
+		nuevo->ant = i;
+	}
+}
+void AgregarOrden(pnodo &inicio, pnodo nuevo) {
+	pnodo i;
+	if (inicio == NULL) {
+		inicio = nuevo;
+	} else if (nuevo->dato < inicio->dato) {
+		nuevo->sig = inicio;
+		inicio->ant = nuevo;
+		inicio = nuevo;
+	} else {
+		for (i = inicio; i->sig != NULL && (i->sig)->dato < nuevo->dato; i = i->sig);
+		if (i->sig != NULL) {
+			nuevo->sig = i->sig;
+			nuevo->ant = i;
+			(i->sig)->ant = nuevo;
+			i->sig = nuevo;
+		} else {
+			i->sig = nuevo;
+			nuevo->ant = i;
+		}
+	}
+}
