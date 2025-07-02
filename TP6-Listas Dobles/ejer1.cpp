83552d1d4af2f2e18bf6ecfa988111c73be249e7
abrilcarrillo <carrillomarianelaabril@gmail.com>
2025-07-01 22:49:32 -0300

    ejer1-commit 5: Quitar: inicio, final y nodo

--- TP6-Listas Dobles/ejer1.cpp
+++ TP6-Listas Dobles/ejer1.cpp
@@ -61,3 +61,59 @@ void AgregarOrden(pnodo &inicio, pnodo nuevo) {
 		}
 	}
 }
+pnodo QuitarInicio(pnodo &inicio) {
+	pnodo extraido;
+	if (inicio == NULL) {
+		extraido = NULL;
+	} else if (inicio->sig == NULL) {
+		extraido = inicio;
+		inicio = NULL;
+	} else {
+		extraido = inicio;
+		inicio = inicio->sig;
+		inicio->ant = NULL;
+		extraido->sig = NULL;
+	}
+	return extraido;
+}
+
+pnodo QuitarFinal(pnodo &inicio) {
+	pnodo extraido;
+	if (inicio == NULL) {
+		extraido = NULL;
+	} else {
+		pnodo i;
+		for (i = inicio; i->sig != NULL; i = i->sig);
+		extraido = i;
+		if (i->ant != NULL) {
+			(i->ant)->sig = NULL;
+			i->ant = NULL;
+		} else {
+			inicio = NULL;
+		}
+	}
+	return extraido;
+}
+pnodo QuitarNodo(pnodo &inicio, int valor) {
+	pnodo extraido, i;
+	if (inicio == NULL) {
+		extraido = NULL;
+	} else if (inicio->dato == valor) {
+		extraido = inicio;
+		inicio = inicio->sig;
+		if (inicio != NULL) inicio->ant = NULL;
+		extraido->sig = NULL;
+	} else {
+		for (i = inicio; i != NULL && i->dato != valor; i = i->sig);
+		if (i != NULL) {
+			extraido = i;
+			if (i->ant != NULL) i->ant->sig = i->sig;
+			if (i->sig != NULL) i->sig->ant = i->ant;
+			extraido->sig = NULL;
+			extraido->ant = NULL;
+		} else {
+			extraido = NULL;
+		}
+	}
+	return extraido;
+}
